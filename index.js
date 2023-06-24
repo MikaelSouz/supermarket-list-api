659eca3dc663002cb34f7a4a338edd571af8b539
MikaelSouz <rodrigues.mikael@hotmail.com>
2023-06-24 12:11:29 -0300

    feat: enable cors

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const router = require("./src/routes");
+const routes = require("./src/routes");
 const port = 3333;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 async function connectDB() {
   await mongoose.connect("mongodb://localhost:27017");
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
   connectDB().catch((error) => {
     console.log(`Error connecting database: ${error}`);
   });
-  app.use("/", router);
+  app.use("/", routes);
   console.log(`Example app listening on port ${port}`);
 });
